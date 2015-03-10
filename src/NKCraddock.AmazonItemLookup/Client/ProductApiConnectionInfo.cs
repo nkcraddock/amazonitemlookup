68244809c827758bed5e511d96d3235f0a3373c4
mplacona <marcos.placona@gmail.com>
2015-03-10 14:56:20 +0000

    Add ability to pass in a webservice URI. Defaults to .com for backwards compatibility

--- src/NKCraddock.AmazonItemLookup/Client/ProductApiConnectionInfo.cs
+++ src/NKCraddock.AmazonItemLookup/Client/ProductApiConnectionInfo.cs
@@ -2,8 +2,14 @@
 {
     public class ProductApiConnectionInfo
     {
+        public ProductApiConnectionInfo()
+        {
+            AWSServerUri = "webservices.amazon.com";
+        }
+
         public string AWSAccessKey { get; set; }
         public string AWSSecretKey { get; set; }
         public string AWSAssociateTag { get; set; }
+        public string AWSServerUri { get; set; }
     }
 }
